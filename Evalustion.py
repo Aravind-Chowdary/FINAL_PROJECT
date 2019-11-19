9b1e99c8f26ac46ae7a03326d0988bf86bcb73ac
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-19 22:10:31 +0000

    Evaluation code for schemas

--- Evalustion.py
+++ Evalustion.py
@@ -3,18 +3,23 @@ from random import randrange
 import random
 import string
 
-stringLength=10
-letters = string.ascii_lowercase
-str_1= ''.join(random.choice(letters) for i in range(stringLength))
-print(str_1)
-now = datetime.now()
-print(now)
-#num=input('Enter the no of random numbers to print')
+stringLength=16
+num=10
 i=0
-num=10000
 while i<num:
-    r = randrange(10000)
-    print (r)
+    letters = string.ascii_lowercase
+    str_1= ''.join(random.choice(letters) for i in range(stringLength))
+    print(str_1)
     i +=1
-nw= datetime.now()
-print (nw)
+
+#now = datetime.now()
+#print(now)
+#num=input('Enter the no of random numbers to print')
+#i=0
+#num=10000
+#while i<num:
+    #r = randrange(10000)
+    #print (r)
+    #i +=1
+#nw= datetime.now()
+#print (nw)
