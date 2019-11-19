eec5cc61490c21417f5927314a62349e5585add2
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-19 22:09:55 +0000

    Evaluation code for schemas

--- Dyer.py
+++ Dyer.py
@@ -1,6 +1,7 @@
-
+from  datetime import datetime
 from random import randrange
-
+import random
+import string
 
 def key_gen(l):
 
@@ -8,14 +9,24 @@ def key_gen(l):
     return k
 
 
-def enc(k, m):
-    p=round(k**0.75)
-    r=randrange(p,k-p)
-
-    c=m*k+r
-    return c
-
-
+def enc(k):
+    print("Encryption started")
+    now = datetime.now()
+    print(now)
+    #stringLength = 16
+    num = 100000
+    i = 0
+    while i<num:
+        p=round(k**0.75)
+        r=randrange(p,k-p)
+        #letters = string.ascii_lowercase
+        #m = ''.join(random.choice(letters) for i in range(stringLength))
+        m =randrange(10000)
+        c=m*k+r
+        return c
+    print ("Encryption ended")
+    nw=datetime.now()
+    print (nw)
 def dec(k, c):
     return c/k
 
