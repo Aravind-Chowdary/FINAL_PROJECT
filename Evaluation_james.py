4c6453fab103aed3814fd9db48d37f0acea9ee51
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-25 11:12:54 +0000

    Evaluation code for James scheme

--- Evaluation_james.py
+++ Evaluation_james.py
@@ -1,33 +1,24 @@
-from  datetime import datetime
-from random import randrange
-import random
-import string
+
+from Dyer import *
+import time
+
 
 def key_gen(l):
 
     k=randrange(2**l,2**(l+1))
     return k
 
-
-print("Encryption started")
-now = datetime.now()
-print(now)
-num = 10000001
-i = 0
-while i<num:
-        k=key_gen(48)
-        p=round(k**0.75)
-        r=randrange(p,k-p)
-        m =randrange(10000)
-        k=64
-        c=m*k+r
-        i += 1
-print ("Encryption ended")
-nw=datetime.now()
-print nw
-diff=nw-now
-print (diff)
-def dec(k, c):
-    return c/k
+def eval():
+    i=0
+    k=key_gen(48)
+    total=0
+    num=100000
+    while i<num:
+        m=randrange(10000)
+        start=time.time()
+        enc(k,m)
+        end=time.time()
+        total +=end-start
+    print ("Time taken for the encryption:", total)
 
 
