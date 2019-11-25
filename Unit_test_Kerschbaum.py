4f710548468e42330bdb7ab0ca42ea98b65b896a
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-25 14:00:24 +0000

    Unit test code for the Kerschbaum.

--- Unit_test_Kerschbaum.py
+++ Unit_test_Kerschbaum.py
@@ -1,19 +1,18 @@
 import unittest
 
-
-
-from Kerschbaum import calc_max,  n, T, encrypt, decrypt
+from Kerschbaum import  n, T, encrypt, decrypt
 
 
 class SimpleTestCase(unittest.TestCase):
     n==13
     T=={}
+    from Kerschbaum import calc_max
     max=calc_max(10)
 
     def testConsistency(self):
         x=85785
-        c = encrypt(x,'gary',6,max)
-        d = decrypt(c,T.t)
+        c = encrypt(x,T,-1,max)
+        d = decrypt(c,T)
         assert d ==x
 
 
