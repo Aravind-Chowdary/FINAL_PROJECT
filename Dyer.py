3c2ddbc44e02d6ef2c0d49d22c0cc930e44420b2
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-25 13:57:24 +0000

    Implementaion of DDD scheme

--- Dyer.py
+++ Dyer.py
@@ -1,33 +1,18 @@
-from  datetime import datetime
+
 from random import randrange
-import random
-import string
+
 
 def key_gen(l):
-
     k=randrange(2**l,2**(l+1))
     return k
 
 
-def enc(k):
-    print("Encryption started")
-    now = datetime.now()
-    print(now)
-    #stringLength = 16
-    num = 100000
-    i = 0
-    while i<num:
-        p=round(k**0.75)
-        r=randrange(p,k-p)
-        #letters = string.ascii_lowercase
-        #m = ''.join(random.choice(letters) for i in range(stringLength))
-        m =randrange(10000)
-        c=m*k+r
-        return c
-    print ("Encryption ended")
-    nw=datetime.now()
-    print (nw)
+def enc(k, m):
+    p=round(k**0.75)
+    r=randrange(p,k-p)
+    c=m*k+r
+    return c
+
+
 def dec(k, c):
     return c/k
-
-
