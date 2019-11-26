148b8ce2dbe7c3516b9ce567d95158b87f3bf952
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-26 22:01:46 +0000

    Reading the m value fro the file

--- Evaluation_james.py
+++ Evaluation_james.py
@@ -1,5 +1,6 @@
 
 from Dyer import *
+
 import time
 
 
@@ -8,7 +9,8 @@ k=key_gen(48)
 total=0
 num=100000
 while i<num:
-    m=randrange(10000)
+    f=open('rands.txt')
+    m=f.readline()
     start=time.time()
     enc(k,m)
     end=time.time()
