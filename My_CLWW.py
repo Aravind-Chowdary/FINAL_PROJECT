df89eb40930814c82a567c72ad1f79da27f8da43
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-26 20:19:45 +0000

    Implemented class in CLWW scheme for the object referencing.

--- My_CLWW.py
+++ My_CLWW.py
@@ -2,24 +2,26 @@ from Crypto.Random import random
 from Crypto.Hash import HMAC,SHA256
 
 class chen:
+    h=None
+    k=None
     def __init__(self):
-        h=None
-        k=None
+        self.k=self.key_gen()
+        self.h=HMAC.new(self.k,digestmod=SHA256)
 
     def key_gen(self):
         k=random.get_random_bytes(64)
         return k
 
-    def pre(self, k, m):
-        h=HMAC.new(k,m,digestmod=SHA256)
-        return h.digest()
+    def prf(self, m):
+        self.h.update(m)
+        return int(self.h.digest())
 
     def ore_enc(self, k, m):
         ab = ""
         ct1 =()
         for i in m:
             ab += i
-            ct1 += str((self.pre(ab[:-1], k) + int(ab[-1])) % 3)
+            ct1 += str((self.prf(ab[:-1]) + int(ab[-1])) % 3)
         return ct1
 
     def ore_compare(self, ct1, ct2):
