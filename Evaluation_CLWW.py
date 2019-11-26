62d09603dc59159fe5f769364256898f74dcdeeb
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-26 12:56:57 +0000

    Evaluation code for CLLW scheme

--- Evaluation_CLWW.py
+++ Evaluation_CLWW.py
@@ -1,48 +1,16 @@
-from Crypto.Random import random
-from Crypto.Hash import HMAC, SHA256
-from  datetime import datetime
+from My_CLWW import *
 from random import randrange
+import time
+chen=chen.NEW()
 
-
-def key_gen():
-    k = random.get_random_bytes(64)
-    return k
-
-
-def pre(k, m):
-    h = HMAC.new(k, m, digestmod=SHA256)
-    return h.digestmod()
-
-
-print("Encryption started")
-now = datetime.now()
-print(now)
-num = 1000000001
-i = 0
+i=0
+k=chen.key_gen()
+total=0
+num=100000
 while i<num:
-    def ore_enc(k, m, h):
-        ab = ""
-        ct1 = ()
-        for i in m:
-            ab += i
-            ct1 += str((h(ab[:-1], k) + int(ab[-1])) % 3)
-        return ct1
-    i += 1
-
-print ("Encryption ended")
-nw=datetime.now()
-print nw
-diff=nw-now
-print (diff)
-
-def ore_compare(ct1, ct2):
-    if ct1 == ct2:
-        return 0
-    L = len(ct1)
-    cnt = 0
-    while ct1[cnt] == ct2[cnt]:
-        cnt += 1
-    if (int(ct1[cnt]) + 1) % 3 == int(ct2[cnt]):
-        return -1
-    else:
-        return 1
+    m=randrange(10000)
+    h=chen.pre(k, m)
+    begin=time.time()
+    chen.ore_enc(k,m)
+    end=time.time()
+    total +=end-begin
