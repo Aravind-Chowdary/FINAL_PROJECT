3844e8eaeff44f260289b205eb9a27eb247dcc01
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-30 12:21:14 +0000

    Evaluation code for james scheme

--- Evaluation_james.py
+++ Evaluation_james.py
@@ -3,17 +3,17 @@ from Dyer import *
 
 import time
 
-
 i = 0
 k = key_gen(48)
 total = 0
 num = 10000
 f = open('rands.txt', 'r')
 while i < num:
-    m = f.readline()
-    int(m)
+    w = f.readline()
+    m=int(w)
     start = time.time()
-    enc(k, m)
+    d=enc(k, m)
+
     end = time.time()
     total += end-start
     i += 1
