36765be3bea9109e0a214f1e16752244a2687480
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-11-30 12:53:06 +0000

    Final Test code for the Boldyreva

--- Unit_test_Boldyreva.py
+++ Unit_test_Boldyreva.py
@@ -4,14 +4,15 @@ from Boldyreva import *
 
 
 class SimpleTestCase(unittest.TestCase):
+
     key = OPE.generate_key(32)
     OPE=OPE(key, in_range=ValueRange(1,2**16-1) , out_range=ValueRange(1,2**31-1))
 
-
-    def testConsistency(self,):
-        plaintext=4916684
-        c =OPE.encrypt(plaintext)
-        d = OPE.decrypt(c)
+    def testConsistency(self):
+        plt=49166
+        plaintext=int(plt)
+        c = self.OPE.encrypt(plaintext)
+        d = self.OPE.decrypt(c)
         assert d == plaintext
 
 
