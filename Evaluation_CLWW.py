239098fc3ce318b9185ab35e4da8092c7f045500
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-12-01 19:54:19 +0000

    Evaluation code for CLWW scheme

--- Evaluation_CLWW.py
+++ Evaluation_CLWW.py
@@ -1,17 +1,21 @@
 from My_CLWW import *
-from random import randrange
-import time
-chen=chen.NEW()
 
-i=0
-k=chen.key_gen()
-total=0
-num=100000
-while i<num:
-    m=randrange(10000)
-    h=chen.pre(k, m)
-    begin=time.time()
-    chen.ore_enc(k,m)
-    end=time.time()
-    total +=end-begin
+
+import time
+
+i = 0
+k = key_gen(48)
+total = 0
+num = 10000
+f = open('rands.txt', 'r')
+while i < num:
+    w = f.readline()
+    m=int(w)
+    start = time.time()
+    d=enc(k, m)
+
+    end = time.time()
+    total += end-start
+    i += 1
 print ("Time taken for the encryption:", float(total)/float(num))
+f.close()
