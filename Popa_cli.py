502359d5be24ac4c840220e64e4d568b9ba4ef9b
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-12-03 16:42:22 +0000

    Changes in the client side architecture

--- Popa_cli.py
+++ Popa_cli.py
@@ -1,26 +1,29 @@
 from Crypto.Random import random
 from Crypto.Cipher import AES
-from Popa_ser import OPETree
-
-OPEobj = OPETree()
-def key_gen():
-    key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+from Popa_ser import *
 
 
-def init_vect():
-    iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])  # This needs to be involved in enc function(doubt)
+class pop_cli:
 
+    OPEobj = OPETree()
 
-def enc(key, iv, data):
-    global OPEobj
-    aes = AES.new(key, AES.MODE_CBC, iv)
-    # data = 'hello world 1234'   <- 16 bytes
-    encd = aes.encrypt(data)
-    OPEobj.insert_in_tree(encd)
-    return encd
+    def key_gen(self):
+        key = ''.join(chr(random.randint(0, 0xFF)) for i in range(16))
+        return key
 
+    def init_vect(self):
+        iv = ''.join([chr(random.randint(0, 0xFF)) for i in range(16)])  # This needs to be involved in enc function(doubt)
+        return iv
 
-def dec(key,encd, iv):
-    aes = AES.new(key, AES.MODE_CBC, iv)
-    decd = aes.decrypt(encd)
-    return decd
+    def enc(self,  iv, data):
+        global OPEobj
+        aes = AES.new(self.key_gen(), AES.MODE_CBC, self.init_vect())
+        # data = 'hello world 1234'   <- 16 bytes
+        encd = aes.encrypt(data)
+        self.OPEobj.insert_in_tree(encd)
+        return encd
+
+    def dec(self, key,encd, iv):
+        aes = AES.new(key, AES.MODE_CBC, iv)
+        decd = aes.decrypt(encd)
+        return decd
