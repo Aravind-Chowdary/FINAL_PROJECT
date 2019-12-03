0b433f2e3146f748f80c22ce44cbd16c6cae0253
Aravind-Chowdary <aravindmullapudi@gmail.com>
2019-12-03 16:45:23 +0000

    unit test code for the CLWW

--- Unit_test_CLWW.py
+++ Unit_test_CLWW.py
@@ -5,20 +5,21 @@ from My_CLWW import *
 
 
 class SimpleTestCase(unittest.TestCase):
-    k = key_gen()
+
+    chen = chen()
 
     def testOrderPreserving(self):
+        k = self.chen.key_gen()
         m1 = random.randint(1, 65535)
         m2 = random.randint(1, 65535)
-        c1 = ore_enc(self.k, m1)
-        c2 = ore_enc(self.k, m2)
+        c1 = self.chen.ore_enc(m1)
+        c2 = self.chen.ore_enc(m2)
         if m1 < m2:
-            assert ore_compare(ct1,ct2)==-1
+            assert self.chen.ore_compare(c1, c2) == -1
         if m1 > m2:
-            assert ore_compare(ct1,ct2)==1
+            assert self.chen.ore_compare(c1, c2) == 1
         if m1==m2:
-            assert ore_compare(ct1,ct2)==0
-
+            assert self.chen.ore_compare(c1, c2) == 0
 
 
 if __name__ == "__main__":
