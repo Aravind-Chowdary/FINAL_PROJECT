6db11f87d40f9342babf1fc2dace7313008b3d4c
Aravind-Chowdary <aravindmullapudi@gmail.com>
2020-02-03 20:05:03 +0000

    changes in the class

--- Evaluation_kerschbaum.py
+++ Evaluation_kerschbaum.py
@@ -1,5 +1,29 @@
+
 from Kerschbaum import *
+
 import time
 
-Tree=Tree.NEW()
-max=calc_max(48)
+
+class evalution():
+
+    Ker = Kersch(2, 16)
+
+    def eval(self):
+
+        i = 0
+        total = 0
+        num = 10000
+        f = open('rands.txt', 'r')
+        while i < num:
+            w = f.readline()
+            m=int(w)
+            start = time.time()
+            d=self.Ker.enc(m)
+            end = time.time()
+            total += end-start
+            i += 1
+
+        print ("Time taken for the encryption:", float(total)/float(num))
+
+        f.close()
+        return
