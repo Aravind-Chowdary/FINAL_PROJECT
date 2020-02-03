44cac2da479d8b0ea71b5306ce7e70954c67e530
Aravind-Chowdary <aravindmullapudi@gmail.com>
2020-02-03 20:04:38 +0000

    changes in the test case

--- Unit_test_Kerschbaum.py
+++ Unit_test_Kerschbaum.py
@@ -1,19 +1,15 @@
 import unittest
 
 from Kerschbaum import *
-Kersch=Kersch()
 
 
 class SimpleTestCase(unittest.TestCase):
-    Kersch.n==16
-    Kersch.T=={}
-
-    max=Kersch.calc_max(10)
+    Kersch = Kersch(2, 16)
 
     def testConsistency(self):
         x=85785
-        c = Kersch.encrypt(x,Kersch.T,-1,max)
-        d = Kersch.decrypt(c,Kersch.T)
+        c = self.Kersch.enc(x)
+        d = self.Kersch.dec(c)
         assert d ==x
 
 
